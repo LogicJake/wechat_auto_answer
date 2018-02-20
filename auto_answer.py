7c1ec53750c027b0e4a7061f43451b5621cb9f3e
NepoleonAsus <835410808@qq.com>
2018-02-20 11:25:58 +0800

    搜索答案显示结果

--- auto_answer.py
+++ auto_answer.py
@@ -5,7 +5,9 @@ import json
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
+from urllib.parse import quote
+import string
+import requests
 order = 0
 
 class LoggingEventHandler(FileSystemEventHandler):
@@ -18,7 +20,12 @@ class LoggingEventHandler(FileSystemEventHandler):
 
 
 def start_work():
-    get_data()
+    try:
+        res = get_data()
+        print(res['content'])
+        print(ask(res['content'],res['options']))
+    except:
+        pass
 
 def get_data():
     global order
@@ -31,7 +38,6 @@ def get_data():
                 temp_order = data['data']['feeds'][0]['field']['question']['order']
                 if  temp_order != order and temp_order != 0:
                     order = temp_order
-                    print(order)
                     res = {}
                     res['content'] = data['data']['feeds'][0]['field']['question']['content']
                     res['options'] = data['data']['feeds'][0]['field']['question']['options']
@@ -39,6 +45,18 @@ def get_data():
             except:
                 pass
 
+def ask(question, options):
+    content = ""
+    for i in range(5):   #爬取几页
+        i = i*10
+        url = quote('http://www.baidu.com/s?&pn='+str(i)+'&wd=' + question, safe = string.printable)
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36"}
+        content = content + requests.get(url, headers=headers).text
+    answer = []
+    for option in options:
+        count = content.count(option)
+        answer.append(option + ' [' + str(count) + ']')
+    return answer
 
 if __name__ == "__main__":
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
@@ -52,3 +70,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+    #start_work()
